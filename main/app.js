b8fdf3378d0a357dcf7358d3e11310cd6a7329f7
Ubuntu <ubuntu@ip-172-31-27-151.us-east-2.compute.internal>
2019-10-29 13:13:31 +0000

    HerghelegiuBiancaAndreeaTema1

--- main/app.js
+++ main/app.js
@@ -1,6 +1,33 @@
 function distance(first, second){
-	//TODO: implementați funcția
-	// TODO: implement the function
+	if((Array.isArray(first)!=true) || (Array.isArray(second)!=true))
+	{
+		throw new TypeError("InvalidType")
+		
+	}
+	if(first.length==null ||second.length==null)
+	{
+		return 0;
+	}
+	let unic=new Set(first)
+	first=Array.from(unic)
+	
+	let unic2=new Set(second)
+	second=Array.from(unic2)
+	
+	var k=0;
+for( var i=0; i<first.length;i++)
+for( var j=0;j<second.length;j++) {
+	if(first[i] != second[j]) 
+	  {
+		  k++;
+	  }
+	  if( first[i] !== second[j] && first[i]== second[j])
+	  k+=2; 
+}
+
+return k;
+	
+	
 }
 
 
